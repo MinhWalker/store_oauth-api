59b7d6b4110ac4232d8c90dda9015a5c0f3afabd
minhwalker <minhnh421999@gmail.com>
2021-03-15 01:15:11 +0700

    add cassandra cluster init

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/MinhWalker/store_oauth-api/src/clients/cassandra"
 	"github.com/MinhWalker/store_oauth-api/src/domain/access_token"
 	"github.com/MinhWalker/store_oauth-api/src/utils/errors"
 )
@@ -16,7 +17,13 @@ func NewRepository() DbRepository {
 	return &dbRepository{}
 }
 
-func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.RestErr) {
+func (r *dbRepository) GetById(string) (*access_token.AccessToken, *errors.RestErr) {
+	session , err := cassandra.GetSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
 
+	// TODO: implement get access token from CassandraDB
 	return nil, errors.NewInternalServerError("database connection not implemented yet!")
 }
