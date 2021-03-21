a5d7b3e65b8af74cce7abb5f8124faa9caa34a97
minhwalker <minhnh421999@gmail.com>
2021-03-21 21:06:53 +0700

    Login method add testing

--- src/repository/rest/users_repository.go
+++ src/repository/rest/users_repository.go
@@ -6,16 +6,18 @@ import (
 	"github.com/MinhWalker/store_oauth-api/src/domain/users"
 	"github.com/MinhWalker/store_oauth-api/src/utils/errors"
 	"gopkg.in/go-resty/resty.v2"
+	"io/ioutil"
+	"net/http/httptest"
 	"time"
 )
 
 var(
 	client = resty.New().SetTimeout(100*time.Millisecond).SetHostURL("http://localhost:8080")
-
 )
 
 type RestUserRepository interface {
 	LoginUser(string, string) (*users.User, *errors.RestErr)
+	LoginUserTest(*httptest.ResponseRecorder) (*users.User, *errors.RestErr)
 }
 
 type usersRepository struct {
@@ -30,9 +32,6 @@ func (u *usersRepository) LoginUser(email, password string) (*users.User, *error
 		Email: email,
 		Password: password,
 	}
-
-	bytes, _ := json.Marshal(request)
-	fmt.Println(string(bytes))
 
 	response, _ := client.R().SetBody(request).Post("/users/login")
 
@@ -55,3 +54,31 @@ func (u *usersRepository) LoginUser(email, password string) (*users.User, *error
 	}
 	return &user, nil
 }
+
+//TODO: testing
+func (u *usersRepository) LoginUserTest(res *httptest.ResponseRecorder) (*users.User, *errors.RestErr) {
+	response := res.Result()
+	body, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+
+	if response == nil || body == nil {
+		return nil, errors.NewInternalServerError("invalid rest client response when trying to login user!")
+	}
+	if response.StatusCode >= 300 {
+		fmt.Println(response.Body)
+		var restErr errors.RestErr
+		err := json.Unmarshal(body, &restErr)
+		if err != nil {
+			return nil, errors.NewInternalServerError("invalid error interface when trying to login user!")
+		}
+		return nil, &restErr
+	}
+
+	var user users.User
+	if err := json.Unmarshal(body, &user); err != nil {
+		return nil, errors.NewInternalServerError("error when trying to unmarshal users response")
+	}
+	return &user, nil
+}
+
--- src/repository/rest/users_repository_test.go
+++ src/repository/rest/users_repository_test.go
@@ -1,1 +1,88 @@
 package rest
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/MinhWalker/store_oauth-api/src/domain/users"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	os.Exit(m.Run())
+}
+
+func TestLoginUserTimeoutFromApi(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("POST", "http://localhost:8080/users/login",
+		httpmock.NewStringResponder(200, `{"error": false, "errorMessage": "", "result": []}`))
+
+	repository := usersRepository{}
+
+	user, err := repository.LoginUser("wewwew1", "12345")
+
+	fmt.Println(err)
+	assert.Nil(t, user)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "invalid rest client response when trying to login user!", err.Message)
+}
+
+func TestLoginUserInvalidErrorInterface(t *testing.T) {
+
+}
+
+func TestLoginUserInvalidLoginCredentials(t *testing.T) {
+
+}
+
+func TestLoginUserInvalidUserJsonResponse(t *testing.T) {
+
+}
+
+func TestLoginUserInvalidNoError(t *testing.T) {
+
+}
+
+func TestList_Handle(t *testing.T) {
+	request := users.UserLoginRequest{
+		Email: "wewwew1",
+		Password: "12345",
+	}
+
+	response := users.User{
+		Id:        1,
+		FirstName: "test",
+		LastName:  "test",
+		Email:     "test",
+	}
+
+	reqUser, _ := json.Marshal(request)
+	resUser, _ := json.Marshal(response)
+
+	req := httptest.NewRequest(http.MethodPost, "/users/login", bytes.NewBuffer(reqUser))
+	res := httptest.NewRecorder()
+	res.Body.Write(resUser)
+	res.Header().Set("X-Request-Id", req.Header.Get("X-Request-Id"))
+
+	repository := usersRepository{}
+
+	user, err := repository.LoginUserTest(res)
+
+	fmt.Println(err)
+	assert.Nil(t, user)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "invalid rest client response when trying to login user!", err.Message)
+}
+
+
